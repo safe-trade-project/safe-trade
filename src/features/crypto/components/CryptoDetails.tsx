a5324cb99e4daba4031ecd56e3ec7ac298c6dbb9
Wojciech Artymiak <artymiakwojtek@gmail.com>
2026-01-11 23:56:21 +0100

    feat: add better styles and fix some bugs

--- src/features/crypto/components/CryptoDetails.tsx
+++ src/features/crypto/components/CryptoDetails.tsx
@@ -8,7 +8,6 @@ import { CryptoChart } from "./CryptoChart";
 export const CryptoDetails = () => {
   const { id } = useParams<{ id: string }>();
   const navigate = useNavigate();
-  const [amount, setAmount] = useState<string>("");
   const [message, setMessage] = useState<{
     text: string;
     type: "success" | "error";
@@ -21,10 +20,26 @@ export const CryptoDetails = () => {
     enabled: !!id,
   });
 
+  const [coinAmount, setCoinAmount] = useState("");
+  const [cashAmount, setCashAmount] = useState("");
+
+  const currentPrice = data?.market_data.current_price.usd ?? 0;
+
+  const onCoinChange = (val: string) => {
+    setCoinAmount(val);
+    setCashAmount(String(Number(val) * currentPrice));
+  };
+
+  const onUsdChange = (val: string) => {
+    setCashAmount(val);
+    setCoinAmount(String(Number(val) / currentPrice));
+  };
+
   const handleBuy = () => {
-    if (!data || !amount) return;
-    const numAmount = parseFloat(amount);
-    if (isNaN(numAmount)) {
+    if (!data) return;
+
+    const numAmount = Number(coinAmount);
+    if (!Number.isFinite(numAmount) || numAmount <= 0) {
       setMessage({ text: "Invalid amount", type: "error" });
       return;
     }
@@ -40,9 +55,10 @@ export const CryptoDetails = () => {
     if (result.success) {
       setMessage({
         text: `Successfully bought ${numAmount} ${data.symbol.toUpperCase()}`,
-        type: "success",
+        type: "success"
       });
-      setAmount("");
+      setCoinAmount("");
+      setCashAmount("");
       setPortfolio(getPortfolio());
     } else {
       setMessage({ text: result.error || "Transaction failed", type: "error" });
@@ -50,9 +66,10 @@ export const CryptoDetails = () => {
   };
 
   const handleSell = () => {
-    if (!data || !amount) return;
-    const numAmount = parseFloat(amount);
-    if (isNaN(numAmount)) {
+    if (!data) return;
+
+    const numAmount = Number(coinAmount);
+    if (!Number.isFinite(numAmount) || numAmount <= 0) {
       setMessage({ text: "Invalid amount", type: "error" });
       return;
     }
@@ -70,14 +87,14 @@ export const CryptoDetails = () => {
         text: `Successfully sold ${numAmount} ${data.symbol.toUpperCase()}`,
         type: "success",
       });
-      setAmount("");
+      setCoinAmount("");
+      setCashAmount("");
       setPortfolio(getPortfolio());
     } else {
       setMessage({ text: result.error || "Transaction failed", type: "error" });
     }
   };
 
-  //------------------------------------
   const marketQuery = useQuery({
     queryKey: ["chart", id],
     queryFn: () => fetchMarketChart(id),
@@ -85,10 +102,6 @@ export const CryptoDetails = () => {
   });
 
   const pricesChartData = marketQuery.data?.prices?.map(([ts, price]) => ({ time: ts, price: price })) ?? [];
-
-  // kapitalizacja rynkowa
-  // const marketCapsChartData = marketQuery.data?.market_caps?.map(([ts, price]) => ({ time: ts, price: price })) ?? [];
-  //------------------------------------
 
   if (isLoading) {
     return (
@@ -107,25 +120,23 @@ export const CryptoDetails = () => {
   }
 
   const holding = portfolio.holdings.find((h) => h.coinId === data?.id);
-  const totalValue =
-    amount && data
-      ? parseFloat(amount) * data.market_data.current_price.usd
-      : 0;
+
+  const totalValue = Number.isFinite(Number(coinAmount)) ? Number(coinAmount) * currentPrice : 0;
 
   return (
-    <div className="p-8 max-w-6xl mx-auto min-h-screen flex flex-col">
+    <div className="p-8 max-w-8/12 mx-auto min-h-screen flex flex-col">
       <button
         onClick={() => navigate('/cryptos')}
-        className="mb-6 text-blue-600 hover:text-blue-800 flex items-center gap-2"
+        className="mb-6 text-2xl text-blue-500 hover:text-blue-600 flex items-center gap-2"
       >
         ← Back to Cryptos
       </button>
 
-      <div className=" rounded-lg shadow-lg p-8 bg-background-light	">
-        <div className="flex items-center gap-6 mb-8 border-b pb-6 ">
+      <div className="rounded-lg shadow-lg p-8 bg-background-light">
+        <div className="flex items-center gap-6 mb-8 border-b pb-6">
           <img src={data.image.large} alt={data.name} className="w-24 h-24" />
           <div className="flex-1">
-            <h1 className="text-4xl font-bold mb-2">{data.name}</h1>
+            <h1 className="text-4xl font-bold text-white mb-2">{data.name}</h1>
             <p className="text-xl text-gray-600 uppercase">{data.symbol}</p>
             <p className="text-sm text-gray-500 mt-1">
               Rank #{data.market_cap_rank}
@@ -138,19 +149,11 @@ export const CryptoDetails = () => {
           </div>
           <div className="text-right">
             <p className="text-sm text-gray-500 mb-1">Current Price</p>
-            <p className="text-4xl font-bold">
+            <p className="text-4xl text-white font-bold">
               ${data.market_data.current_price.usd.toLocaleString()}
             </p>
           </div>
         </div>
-
-        {
-          pricesChartData.length > 0 && (
-            <div className="mb-8">
-              <CryptoChart data={pricesChartData} />
-            </div>
-          )
-        }
 
         {message && (
           <div
@@ -160,47 +163,66 @@ export const CryptoDetails = () => {
           </div>
         )}
 
-        <div className="bg-blue-50 rounded-lg p-6 mb-8">
-          <h2 className="text-2xl font-bold mb-4">Trade {data.name}</h2>
-          <div className="flex gap-4 items-end">
-            <div className="flex-1">
-              <label className="block text-sm font-medium text-gray-700 mb-2">
-                Amount ({data.symbol.toUpperCase()})
-              </label>
-              <input
-                type="number"
-                step="any"
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
-                placeholder="0.00"
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              />
-              {amount && (
-                <p className="text-sm text-gray-600 mt-1">
-                  Total: ${totalValue.toFixed(2)}
-                </p>
-              )}
+        <div className="mx-auto flex flex-col lg:flex-row gap-6 mb-6">
+          {pricesChartData.length > 0 && (
+            <div className="flex-1 min-w-0 p-6">
+              <CryptoChart data={pricesChartData} />
             </div>
-            <button
-              onClick={handleBuy}
-              disabled={!amount}
-              className="px-6 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 disabled:bg-gray-400 disabled:cursor-not-allowed font-semibold"
-            >
-              Buy
-            </button>
-            <button
-              onClick={handleSell}
-              disabled={!amount}
-              className="px-6 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 disabled:bg-gray-400 disabled:cursor-not-allowed font-semibold"
-            >
-              Sell
-            </button>
+          )}
+          <div className="mt-6 max-w-1/4 bg-white/3 rounded-lg p-6 flex flex-col h-full">
+            <h2 className="text-white text-2xl font-bold mb-4">Trade {data.name}</h2>
+            <div className="flex flex-col gap-4 flex-1">
+              <div className="flex-1">
+                <label className="block text-sm font-semibold text-gray-300 mb-2">
+                  Amount ({data.symbol.toUpperCase()})
+                </label>
+                <input
+                  type="text"
+                  inputMode="decimal"
+                  value={coinAmount}
+                  onChange={(e) => onCoinChange(e.target.value.replace(",", "."))}
+                  placeholder="0.00"
+                  className="w-full text-white px-4 py-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+
+                <p className="block text-sm font-semibold text-gray-300 mb-2">1{data.symbol.toUpperCase()} =~ ${currentPrice.toLocaleString()} USD</p>
+              </div>
+              <div className="flex-1">
+                <label className="block text-sm font-semibold text-gray-300 mb-2">
+                  Amount USD
+                </label>
+                <input
+                  type="text"
+                  inputMode="decimal"
+                  value={cashAmount}
+                  onChange={(e) => onUsdChange(e.target.value)}
+                  placeholder={totalValue.toFixed(2)}
+                  className="w-full text-white px-4 py-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+              <div className="mt-auto flex flex-col gap-1">
+                <button
+                  onClick={handleBuy}
+                  disabled={!coinAmount || Number(coinAmount) <= 0}
+                  className="px-6 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 disabled:bg-gray-400 disabled:cursor-not-allowed font-semibold"
+                >
+                  Buy
+                </button>
+                <button
+                  onClick={handleSell}
+                  disabled={!coinAmount || Number(coinAmount) <= 0}
+                  className="px-6 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 disabled:bg-gray-400 disabled:cursor-not-allowed font-semibold"
+                >
+                  Sell
+                </button>
+              </div>
+            </div>
           </div>
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-8">
           <div className="bg-white/3 rounded-lg p-4 ">
-            <p className="text-sm text-gray-600 mb-2 text-white font-bold">
+            <p className="text-sm mb-2 text-white font-bold">
               24h Change
             </p>
             <p
@@ -211,7 +233,7 @@ export const CryptoDetails = () => {
             </p>
           </div>
           <div className="bg-white/3 rounded-lg p-4">
-            <p className="text-sm text-gray-600 mb-2 text-white font-bold">
+            <p className="text-sm mb-2 text-white font-bold">
               7d Change
             </p>
             <p
@@ -222,7 +244,7 @@ export const CryptoDetails = () => {
             </p>
           </div>
           <div className="bg-white/3 rounded-lg p-4">
-            <p className="text-sm text-gray-600 mb-2 text-white font-bold">
+            <p className="text-sm mb-2 text-white font-bold">
               30d Change
             </p>
             <p
@@ -238,25 +260,25 @@ export const CryptoDetails = () => {
           <div className="space-y-4">
             <div className="flex justify-between border-b pb-2">
               <span className="text-primary">Market Cap</span>
-              <span className="font-semibold">
+              <span className="font-semibold text-white">
                 ${data.market_data.market_cap.usd.toLocaleString()}
               </span>
             </div>
             <div className="flex justify-between border-b pb-2">
               <span className="text-primary">24h Volume</span>
-              <span className="font-semibold">
+              <span className="font-semibold text-white">
                 ${data.market_data.total_volume.usd.toLocaleString()}
               </span>
             </div>
             <div className="flex justify-between border-b pb-2">
               <span className="text-primary">24h High</span>
-              <span className="font-semibold">
+              <span className="font-semibold text-white">
                 ${data.market_data.high_24h.usd.toLocaleString()}
               </span>
             </div>
             <div className="flex justify-between border-b pb-2">
               <span className="text-primary">24h Low</span>
-              <span className="font-semibold">
+              <span className="font-semibold text-white">
                 ${data.market_data.low_24h.usd.toLocaleString()}
               </span>
             </div>
@@ -264,13 +286,13 @@ export const CryptoDetails = () => {
           <div className="space-y-4">
             <div className="flex justify-between border-b pb-2">
               <span className="text-primary">Circulating Supply</span>
-              <span className="font-semibold">
+              <span className="font-semibold text-white">
                 {data.market_data.circulating_supply.toLocaleString()}
               </span>
             </div>
             <div className="flex justify-between border-b pb-2">
               <span className="text-primary">Total Supply</span>
-              <span className="font-semibold">
+              <span className="font-semibold text-white">
                 {data.market_data.total_supply
                   ? data.market_data.total_supply.toLocaleString()
                   : "N/A"}
@@ -278,7 +300,7 @@ export const CryptoDetails = () => {
             </div>
             <div className="flex justify-between border-b pb-2">
               <span className="text-primary">Max Supply</span>
-              <span className="font-semibold">
+              <span className="font-semibold text-white">
                 {data.market_data.max_supply
                   ? data.market_data.max_supply.toLocaleString()
                   : "N/A"}
@@ -289,9 +311,9 @@ export const CryptoDetails = () => {
 
         {data.description.en && (
           <div className="border-t pt-6">
-            <h2 className="text-2xl font-bold mb-4 text-">About {data.name}</h2>
+            <h2 className="text-2xl font-bold text-white mb-4 text-">About {data.name}</h2>
             <div
-              className="text-gray-700 leading-relaxed text-white/70"
+              className="leading-relaxed text-white/70"
               dangerouslySetInnerHTML={{ __html: data.description.en }}
             />
           </div>
